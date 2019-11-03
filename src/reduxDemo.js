81d462be3de84d29eec48f19bc8612f89e46d3ea
grabstacker <bradkay12@gmail.com>
2019-11-03 18:08:44 +1200

    combining reducers

--- src/reduxDemo.js
+++ src/reduxDemo.js
@@ -1,24 +1,46 @@
 import React, {Component} from 'react'
-import { createStore } from 'redux'
+import {combineReducers, createStore } from 'redux'
 
 class ReduxDemo extends Component {
 
     render() {
 
-        const reducer = function(state, action){
+        const attackReducer = function(state = "", action){
+switch (action.type){
+    case "ATTACK":
+        return action.payload
+        case "GREENATTACK":
+            return action.payload
+            case "quickAttack":
+                return action.payload
+}
+            return state;
+        }
+
+        const defenceReducer = function(state = "", action){
             if(action.type === "ATTACK"){
                 return action.payload
             }
             if(action.type === "GREENATTACK"){
-                return action.payload
+                return action.payload + " mouse"
             }
             return state;
         }
+
+        const combatReducer = combineReducers({
+            attack: attackReducer,
+            defence: defenceReducer
+        })
         //create store
-        const store = createStore(reducer, "Peace")
+        const store = createStore(combatReducer,{
+            attack: "ATTACK",
+            defence: "ATTACK"
+        },
+        window.devToolsExtension && window.devToolsExtension()
+        )
         //create an reducer
 
-
+       
         //subscribe
         store.subscribe(() => {
             console.log("store is now ", store.getState())
@@ -26,9 +48,14 @@ class ReduxDemo extends Component {
 
         // dispatch
 
-        store.dispatch({type: "ATTACK", payload: "IronMan"})
+        store.dispatch({type: "ATTACK", payload: "IronMan"}) //passing an object with action and payload to dispatch
         store.dispatch({type: "GREENATTACK", payload: "HULK"})
 
+        const updateAttack = {
+            type: "quickAttack",
+            payload: "slice"
+        }
+        store.dispatch(updateAttack)
         return(
             <div>
                 test
